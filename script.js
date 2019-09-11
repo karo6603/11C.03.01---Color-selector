90a0bdfa89da7ff1e93a622388528d9bf0402371
Karoline Krogsbøll <you@example.com>
2019-09-11 22:52:03 +0200

    everything works

--- script.js
+++ script.js
@@ -3,13 +3,68 @@ document.addEventListener("DOMContentLoaded", start);
 
 function start() {
   document.querySelector("input").addEventListener("input", selectColor);
+  document.querySelector("select").addEventListener("change", selectTheme);
 
   function selectColor(event) {
-    let color = event.target.value;
-    hexToRgb(color);
+    setColors(event.target.value);
   }
 
-  function hexToRgb(color) {
+  function selectTheme() {
+    setColors(document.querySelector("input").value);
+  }
+
+  function RGBToHex(rgb) {
+    var hex = rgb.r.toString(16) + rgb.g.toString(16) + rgb.b.toString(16);
+    if (hex.length < 2) {
+      hex = "0" + hex;
+    }
+    return hex;
+  }
+
+  function HSLToRGB(h, s, l) {
+    s /= 100;
+    l /= 100;
+
+    let c = (1 - Math.abs(2 * l - 1)) * s,
+      x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+      m = l - c / 2,
+      r = 0,
+      g = 0,
+      b = 0;
+
+    if (0 <= h && h < 60) {
+      r = c;
+      g = x;
+      b = 0;
+    } else if (60 <= h && h < 120) {
+      r = x;
+      g = c;
+      b = 0;
+    } else if (120 <= h && h < 180) {
+      r = 0;
+      g = c;
+      b = x;
+    } else if (180 <= h && h < 240) {
+      r = 0;
+      g = x;
+      b = c;
+    } else if (240 <= h && h < 300) {
+      r = x;
+      g = 0;
+      b = c;
+    } else if (300 <= h && h < 360) {
+      r = c;
+      g = 0;
+      b = x;
+    }
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return { r, g, b };
+  }
+
+  function hexToRGB(color) {
     let hex;
     if (color.substring(0, 1) == "#") {
       hex = color.substring(1);
@@ -19,18 +74,14 @@ function start() {
     rgb.g = parseInt(hex.substring(2, 4), 16);
     rgb.b = parseInt(hex.substring(4), 16);
 
-    rgbToHsl(color, rgb);
+    return rgb;
   }
 
-  function rgbToHsl(color, rgb) {
+  function RGBToHSL(rgb) {
     const rgbConvert = {};
-    rgbConvert.r = rgb.r;
-    rgbConvert.g = rgb.g;
-    rgbConvert.b = rgb.b;
-
-    rgbConvert.r /= 255;
-    rgbConvert.g /= 255;
-    rgbConvert.b /= 255;
+    rgbConvert.r = rgb.r / 255;
+    rgbConvert.g = rgb.g / 255;
+    rgbConvert.b = rgb.b / 255;
 
     let h, s, l;
 
@@ -62,107 +113,94 @@ function start() {
     s *= 100;
     l *= 100;
 
-    const hsl = `${Math.floor(h)}, ${Math.floor(s)}%, ${Math.floor(l)}%`;
+    return { h: Math.round(h), s: Math.round(s), l: Math.round(l) };
+  }
 
-    h = Math.round(h);
-    s = Math.round(s);
-    l = Math.round(l);
-
-    showColor(color, rgb, hsl);
-
-    if (document.querySelector("#color_picker").value == "analogous") {
-      setAnalogous(h, s, l);
+  function setColors(hex) {
+    const rgb = hexToRGB(hex);
+    const hsl = RGBToHSL(rgb);
+    if (document.querySelector("#theme_picker").value == "analogous") {
+      setAnalogous(hsl);
     } else if (
-      document.querySelector("#color_picker").value == "monochromatic"
+      document.querySelector("#theme_picker").value == "monochromatic"
     ) {
-      setMonoChromatic(h, s, l);
-    } else if (document.querySelector("#color_picker").value == "triad") {
-      setTriad(h, s, l);
+      setMonoChromatic(hsl);
+    } else if (document.querySelector("#theme_picker").value == "triad") {
+      setTriad(hsl);
     } else if (
-      document.querySelector("#color_picker").value == "complementary"
+      document.querySelector("#theme_picker").value == "complementary"
     ) {
-      setComplementary(h, s, l);
-    } else if (document.querySelector("#color_picker").value == "compound") {
-      setCompound(h, s, l);
-    } else if (document.querySelector("#color_picker").value == "shades") {
-      setShades(h, s, l);
+      setComplementary(hsl);
+    } else if (document.querySelector("#theme_picker").value == "compound") {
+      setCompound(hsl);
+    } else if (document.querySelector("#theme_picker").value == "shades") {
+      setShades(hsl);
     }
   }
 
-  function setAnalogous(h, s, l) {
-    console.log("hej");
-
-    document.querySelector(".box2").style.backgroundColor = `hsl(${h -
-      60},${s}%,${l}%)`;
-    document.querySelector(".box3").style.backgroundColor = `hsl(${h -
-      30}, ${s}%, ${l}%)`;
-    document.querySelector(".box4").style.backgroundColor = `hsl(${h +
-      9}, ${s}%, ${l}%)`;
-    document.querySelector(".box5").style.backgroundColor = `hsl(${h +
-      18}, ${s}%, ${l}%)`;
+  function setAnalogous(hsl) {
+    showColor(hsl.h, hsl.s, hsl.l, 1);
+    showColor(hsl.h - 60, hsl.s, hsl.l, 2);
+    showColor(hsl.h - 30, hsl.s, hsl.l, 3);
+    showColor(hsl.h + 9, hsl.s, hsl.l, 4);
+    showColor(hsl.h + 18, hsl.s, hsl.l, 5);
   }
 
-  function setMonoChromatic(h, s, l) {
-    document.querySelector(".box2").style.background = `hsl(${h}, ${s}%, ${l -
-      5}%)`;
-    document.querySelector(".box3").style.background = `hsl(${h}, ${s}%, ${l -
-      10}%)`;
-    document.querySelector(".box4").style.background = `hsl(${h}, ${s}%, ${l -
-      15}%)`;
-    document.querySelector(".box5").style.background = `hsl(${h}, ${s}%, ${l -
-      20}%)`;
+  function setMonoChromatic(hsl) {
+    showColor(hsl.h, hsl.s, hsl.l, 1);
+    showColor(hsl.h, hsl.s, hsl.l - 5, 2);
+    showColor(hsl.h, hsl.s, hsl.l - 10, 3);
+    showColor(hsl.h, hsl.s, hsl.l - 15, 4);
+    showColor(hsl.h, hsl.s, hsl.l - 20, 5);
   }
 
-  function setTriad(h, s, l) {
-    document.querySelector(".box2").style.background = `hsl(${h -
-      60}, ${s}%, ${l}%)`;
-    document.querySelector(".box3").style.background = `hsl(${h -
-      120}, ${s}%, ${l}%)`;
-    document.querySelector(".box4").style.background = `hsl(${h -
-      120}, ${s}%, ${l}%)`;
-    document.querySelector(".box5").style.background = `hsl(${h -
-      60}, ${s}%, ${l}%)`;
+  function setTriad(hsl) {
+    showColor(hsl.h, hsl.s, hsl.l, 1);
+    showColor(hsl.h - 60, hsl.s, hsl.l, 2);
+    showColor(hsl.h - 120, hsl.s, hsl.l, 3);
+    showColor(hsl.h - 120, hsl.s, hsl.l, 4);
+    showColor(hsl.h - 60, hsl.s, hsl.l, 5);
   }
 
-  function setComplementary(h, s, l) {
-    document.querySelector(".box2").style.background = `hsl(${h -
-      180}, ${s}%, ${l}%)`;
-    document.querySelector(".box3").style.background = `hsl(${h -
-      170}, ${s}%, ${l}%)`;
-    document.querySelector(".box4").style.background = `hsl(${h -
-      160}, ${s}%, ${l}%)`;
-    document.querySelector(".box5").style.background = `hsl(${h -
-      190}, ${s}%, ${l}%)`;
+  function setComplementary(hsl) {
+    showColor(hsl.h, hsl.s, hsl.l, 1);
+    showColor(hsl.h - 180, hsl.s, hsl.l, 2);
+    showColor(hsl.h - 170, hsl.s, hsl.l, 3);
+    showColor(hsl.h - 160, hsl.s, hsl.l, 4);
+    showColor(hsl.h - 190, hsl.s, hsl.l, 5);
   }
 
-  function setCompound(h, s, l) {
-    document.querySelector(".box2").style.background = `hsl(${h -
-      180}, ${s}%, ${l}%)`;
-    document.querySelector(".box3").style.background = `hsl(${h -
-      150}, ${s}%, ${l - 10}%)`;
-    document.querySelector(".box4").style.background = `hsl(${h +
-      150}, ${s}%, ${l}%)`;
-    document.querySelector(".box5").style.background = `hsl(${h -
-      180}, ${s}%, ${l - 10}%)`;
+  function setCompound(hsl) {
+    showColor(hsl.h, hsl.s, hsl.l, 1);
+    showColor(hsl.h - 180, hsl.s, hsl.l, 2);
+    showColor(hsl.h - 150, hsl.s, hsl.l, 3);
+    showColor(hsl.h + 150, hsl.s, hsl.l, 4);
+    showColor(hsl.h - 80, hsl.s, hsl.l, 5);
   }
 
-  function setShades(h, s, l) {
-    document.querySelector(".box2").style.background = `hsl(${h}, ${s -
-      5}%, ${l}%)`;
-    document.querySelector(".box3").style.background = `hsl(${h}, ${s -
-      10}%, ${l - 10}%)`;
-    document.querySelector(".box4").style.background = `hsl(${h}, ${s -
-      15}%, ${l}%)`;
-    document.querySelector(".box5").style.background = `hsl(${h}, ${s -
-      20}%, ${l - 10}%)`;
+  function setShades(hsl) {
+    showColor(hsl.h, hsl.s, hsl.l, 1);
+    showColor(hsl.h, hsl.s - 5, hsl.l, 2);
+    showColor(hsl.h, hsl.s - 10, hsl.l - 10, 3);
+    showColor(hsl.h, hsl.s - 15, hsl.l, 4);
+    showColor(hsl.h, hsl.s - 20, hsl.l - 10, 5);
   }
 
-  function showColor(color, rgb, hsl) {
-    document.querySelector(".box1").style.backgroundColor = color;
-    document.querySelector(".hex-output").textContent = `HEX: ${color}`;
+  function showColor(h, s, l, counter) {
+    const rgb = HSLToRGB(h, s, l);
     document.querySelector(
-      ".rgb-output"
+      ".box" + counter
+    ).style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
+    document.querySelector(
+      ".hex-output" + counter
+    ).textContent = `HEX: ${RGBToHex(rgb)}`;
+    document.querySelector(
+      ".rgb-output" + counter
     ).textContent = `RGB: ${rgb.r}, ${rgb.g}, ${rgb.b}`;
-    document.querySelector(".hsl-output").textContent = `HSL: ${hsl}`;
+    document.querySelector(
+      ".hsl-output" + counter
+    ).textContent = `HSL: ${h}${s}${l}`;
   }
+
+  selectTheme();
 }
